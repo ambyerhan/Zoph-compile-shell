b30e300eb5cfaedcc07cb4bc6722450f00700e6f
amin <amberhan0301@163.com>
2018-06-20 09:38:11 +0800

    update dec

--- dec.sh
+++ dec.sh
@@ -1,11 +1,12 @@
 
-DATA_DIR=./data
+DATA_DIR=./DATADIR
 TRAIN_DIR=./TRAINDIR
 TMP_DIR=./TMPDIR
-BEST_DIR=./BEST
+BEST_DIR=./BEST/best.nn_save_all_models_15.nn
 INFER=./INFER/infer.txt
 OUTPUT=./1best.txt
 
+GPU=3
 BEAM=4
 BATCH=80
 EPOCH=10
@@ -20,10 +21,12 @@ SVOC=30000
 TVOC=30000
 
 ./DEBUG \
-	-k 1 $BEST_DIR $OUTPUT 
-	--decode-main-data-files $INFER 
-	-L 200
-	-b $BEAM
-	--print-score true 
-	--dec-ratio 0.2 1.8
+	-k 1 $BEST_DIR $OUTPUT \
+	--decode-main-data-files $INFER \
+	-L 200 \
+	-b $BEAM \
+	--print-score true \
+	--dec-ratio 0.2 1.8 \
+	--tmp-dir-location $TMP_DIR \
+	-M $GPU
 
